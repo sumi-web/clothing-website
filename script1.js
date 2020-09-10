301029e3ce7a4031d58d5d738bf65413c6d37aaf
sumit <sumity159@gmail.com>
2020-09-10 16:09:37 +0530

    second commit

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 //************new arrivals************
 const arrivals = [{
     id: "men",
-    image1: "images/a8acac3a-077a-400f-a1c7-519215a5265e1537251707732-Roadster-Men-Blue-Denim-Washed-Casual-Shirt-5281537251705552-1.jpg",
+    image1: "Images/a8acac3a-077a-400f-a1c7-519215a5265e1537251707732-Roadster-Men-Blue-Denim-Washed-Casual-Shirt-5281537251705552-1.jpg",
     name1: "Casual Denim Shirt",
     price1: 900,
 
@@ -16,7 +16,7 @@ const arrivals = [{
     price3: 999,
 
     id: "men",
-    image4: "images/db444c48-c8aa-4fc3-92b4-02b66f4643dd1545820130218-Armani-Exchange-Men-Black-Analogue-Watch-6211545820128996-1.webp",
+    image4: "Images/db444c48-c8aa-4fc3-92b4-02b66f4643dd1545820130218-Armani-Exchange-Men-Black-Analogue-Watch-6211545820128996-1.webp",
     name4: "Analog Watch",
     price4: 4299,
 
@@ -33,94 +33,94 @@ const arrivals = [{
 
   {
     id: "women",
-    image1: "images/8df4ed41-1e43-4a0d-97fe-eb47edbdbacd1556086871124-Libas-Women-Kurtas-6161556086869769-1.webp",
+    image1: "Images/8df4ed41-1e43-4a0d-97fe-eb47edbdbacd1556086871124-Libas-Women-Kurtas-6161556086869769-1.webp",
     name1: "Print Straight Kurta",
     price1: 900,
 
     id: "women",
-    image2: "images/11522735513998-HRX-by-Hrithik-Roshan-Black-Joggers-2821522735513847-1.jpg",
+    image2: "Images/11522735513998-HRX-by-Hrithik-Roshan-Black-Joggers-2821522735513847-1.jpg",
     name2: "Lenght Track Pants",
     price2: 800,
 
     id: "women",
-    image3: "images/f979e44a-d583-447c-bfa4-cfa354fe18621569581591523-HRX-by-Hrithik-Roshan-Women-Tshirts-1641569581590485-1.webp",
+    image3: "Images/f979e44a-d583-447c-bfa4-cfa354fe18621569581591523-HRX-by-Hrithik-Roshan-Women-Tshirts-1641569581590485-1.webp",
     name3: "Running T-shirt",
     price3: 999,
 
     id: "women",
-    image4: "images/e73eebe5-b63f-4643-a2d6-cbf39fdaec6f1570882139845-DressBerry-Women-Coats-1761570882137262-1.jpg",
+    image4: "Images/e73eebe5-b63f-4643-a2d6-cbf39fdaec6f1570882139845-DressBerry-Women-Coats-1761570882137262-1.jpg",
     name4: "Mustard Solid Coat",
     price4: 4299,
 
     id: "women",
-    image5: "images/b64bbae7-f220-46e3-9246-aad5aaab69171579040040721-1.webp",
+    image5: "Images/b64bbae7-f220-46e3-9246-aad5aaab69171579040040721-1.webp",
     name5: "Silk Slim Joggers",
     price5: 899,
 
     id: "women",
-    image6: "images/ede596ec-8c52-4f11-9210-df139900cec21540883446206-Mitera-Black-Silk-Blend-Banarasi-Ethnic-Solid-Woven-Saree-with-Matching-Blouse-8411540883445971-1.webp",
+    image6: "Images/ede596ec-8c52-4f11-9210-df139900cec21540883446206-Mitera-Black-Silk-Blend-Banarasi-Ethnic-Solid-Woven-Saree-with-Matching-Blouse-8411540883445971-1.webp",
     name6: "Solid Banarasi Saree",
     price6: 2359,
   },
   {
     id: "kid",
-    image1: "images/f4edb564-c5a9-4292-b47a-00464c86f64f1579732582431-1.webp",
+    image1: "Images/f4edb564-c5a9-4292-b47a-00464c86f64f1579732582431-1.webp",
     name1: "Clean Look Jeans",
     price1: 900,
 
     id: "kid",
-    image2: "images/ac850d4b-8e6e-4067-b7eb-4ea9e961e4d11534486603914-StyleStone-Girls-Blue-Printed-Fit-and-Flare-Dress-1311534486603825-1.webp",
+    image2: "Images/ac850d4b-8e6e-4067-b7eb-4ea9e961e4d11534486603914-StyleStone-Girls-Blue-Printed-Fit-and-Flare-Dress-1311534486603825-1.webp",
     name2: "Printed Flare Dress",
     price2: 800,
 
     id: "kid",
-    image3: "images/0382daed-ffff-48b9-879f-4139dc0901591597639846624-Sweet-Dreams-Boys-Peach-Coloured--Green-Printed-Night-suit-2-1.jpg",
+    image3: "Images/0382daed-ffff-48b9-879f-4139dc0901591597639846624-Sweet-Dreams-Boys-Peach-Coloured--Green-Printed-Night-suit-2-1.jpg",
     name3: "Printed Night Suit",
     price3: 999,
 
     id: "kid",
-    image4: "images/64d68229-ade1-431c-8bc3-422486d8a6b31594975298137-YK-Basics-Boys-Pack-of-5-Round-Neck-T-shirts-223159497529597-1.webp",
+    image4: "Images/64d68229-ade1-431c-8bc3-422486d8a6b31594975298137-YK-Basics-Boys-Pack-of-5-Round-Neck-T-shirts-223159497529597-1.webp",
     name4: "5 rounded T-shirt",
     price4: 4299,
 
     id: "kid",
-    image5: "images/22e77ac8-1a1a-4bdb-9c5c-eb5a84daa7cd1598617208585LilPicksBoysPinkSolidStraightKurta1.webp",
+    image5: "Images/22e77ac8-1a1a-4bdb-9c5c-eb5a84daa7cd1598617208585LilPicksBoysPinkSolidStraightKurta1.webp",
     name5: "Solid Straight Kurta",
     price5: 899,
 
     id: "kid",
-    image6: "images/a5832b21-6c11-4076-8f46-3e4f3287bd5d1546649284259-1.webp",
+    image6: "Images/a5832b21-6c11-4076-8f46-3e4f3287bd5d1546649284259-1.webp",
     name6: "Printed Kurta and Plazzos ",
     price6: 2359,
   },
   {
     id: "footwear",
-    image1: "images/1c077b32-6f5b-4776-b97c-55019b311ded1576578225554-Anouk-Women-Off-White-Beaded-Mojaris-2241576578224391-1.webp",
+    image1: "Images/1c077b32-6f5b-4776-b97c-55019b311ded1576578225554-Anouk-Women-Off-White-Beaded-Mojaris-2241576578224391-1.webp",
     name1: "Off-White Beaded Mojaris",
     price1: 900,
 
     id: "footwear",
-    image2: "images/5fd89d7d-31e7-49cd-ad6f-f987182fd13e1582783276085-Skechers-Women-Navy-Blue--Pink-GO-RUN-RIDE-8-Running-Shoes-9-1.webp",
+    image2: "Images/5fd89d7d-31e7-49cd-ad6f-f987182fd13e1582783276085-Skechers-Women-Navy-Blue--Pink-GO-RUN-RIDE-8-Running-Shoes-9-1.webp",
     name2: "Pink GO RIDE Running Shoes",
     price2: 800,
 
     id: "footwear",
-    image3: "images/8d1bb62a-a4c8-4339-bbda-9a49c5d9d6f71559973009336-Crew-STREET-Women-Pink-Running-Shoes-9841559973008284-1.jpg",
+    image3: "Images/8d1bb62a-a4c8-4339-bbda-9a49c5d9d6f71559973009336-Crew-STREET-Women-Pink-Running-Shoes-9841559973008284-1.jpg",
     name3: "Colourblocked Running Shoes",
     price3: 999,
 
     id: "footwear",
-    image4: "images/e00d86e1-7e0c-46d7-8e01-1c721ca06e281590079932688PumaMenNavyBlueRedEscaperStarIDPSneakers1.webp",
+    image4: "Images/e00d86e1-7e0c-46d7-8e01-1c721ca06e281590079932688PumaMenNavyBlueRedEscaperStarIDPSneakers1.webp",
     name4: " Blue & Red Escaper Star IDP Sneakers",
     price4: 4299,
 
     id: "footwear",
-    image5: "images/52f6a65d-301e-456e-b4d4-851255db49091572344975999-MetallicSquare-ToeSandals-221572344975143-1.webp",
+    image5: "Images/52f6a65d-301e-456e-b4d4-851255db49091572344975999-MetallicSquare-ToeSandals-221572344975143-1.webp",
     name5: "Transparent Solid Sandals",
     price5: 899,
 
     id: "footwear",
-    image6: "images/9155a144-fad7-488b-a338-58c50bb844c61579848278081-Nike-Men-Red-AIR-MAX-IMPACT-Basketball-Shoes-704157984827574-1.webp",
+    image6: "Images/9155a144-fad7-488b-a338-58c50bb844c61579848278081-Nike-Men-Red-AIR-MAX-IMPACT-Basketball-Shoes-704157984827574-1.webp",
     name6: "Mid-Top Basketball Shoes",
     price6: 2359,
   },
