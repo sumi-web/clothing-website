f7d8e44371c347e8f57c5d3e7873a6993a5e7008
sumit <sumity159@gmail.com>
2020-09-10 13:04:46 +0530

    first commit

---
+++ script1.js
@@ -0,0 +1,243 @@
+//************new arrivals************
+const arrivals = [{
+    id: "men",
+    image1: "images/a8acac3a-077a-400f-a1c7-519215a5265e1537251707732-Roadster-Men-Blue-Denim-Washed-Casual-Shirt-5281537251705552-1.jpg",
+    name1: "Casual Denim Shirt",
+    price1: 900,
+
+    id: "men",
+    image2: "Images/60785496-7974-463b-b604-634d33ce6cde1599120622296-BEAT-LONDON-by-PEPE-JEANS-Men-Jeans-6291599120620590-1.jpg",
+    name2: "Slim Fit Jeans",
+    price2: 800,
+
+    id: "men",
+    image3: "Images/7b9db4fb-317c-4ce9-ae30-e7522a89911d1568451555200-HERENOW-Men-Sweatshirts-6561568451553655-1.webp",
+    name3: "Printed Sweatshirts",
+    price3: 999,
+
+    id: "men",
+    image4: "images/db444c48-c8aa-4fc3-92b4-02b66f4643dd1545820130218-Armani-Exchange-Men-Black-Analogue-Watch-6211545820128996-1.webp",
+    name4: "Analog Watch",
+    price4: 4299,
+
+    id: "men",
+    image5: "Images/11449479796670-INVICTUS-Red--Navy-Checked-Slim-Formal-Shirt-4621449479796242-1.webp",
+    name5: "Checked Formal Shirt",
+    price5: 899,
+
+    id: "men",
+    image6: "Images/d0945f5f-bb57-45a6-9615-7aea74707d201586861228235-ADIDAS-Men-Navy-Sedna-Running-Shoes-7821586861226106-1.webp",
+    name6: "Running Shoes",
+    price6: 2359,
+  },
+
+  {
+    id: "women",
+    image1: "images/8df4ed41-1e43-4a0d-97fe-eb47edbdbacd1556086871124-Libas-Women-Kurtas-6161556086869769-1.webp",
+    name1: "Print Straight Kurta",
+    price1: 900,
+
+    id: "women",
+    image2: "images/11522735513998-HRX-by-Hrithik-Roshan-Black-Joggers-2821522735513847-1.jpg",
+    name2: "Lenght Track Pants",
+    price2: 800,
+
+    id: "women",
+    image3: "images/f979e44a-d583-447c-bfa4-cfa354fe18621569581591523-HRX-by-Hrithik-Roshan-Women-Tshirts-1641569581590485-1.webp",
+    name3: "Running T-shirt",
+    price3: 999,
+
+    id: "women",
+    image4: "images/e73eebe5-b63f-4643-a2d6-cbf39fdaec6f1570882139845-DressBerry-Women-Coats-1761570882137262-1.jpg",
+    name4: "Mustard Solid Coat",
+    price4: 4299,
+
+    id: "women",
+    image5: "images/b64bbae7-f220-46e3-9246-aad5aaab69171579040040721-1.webp",
+    name5: "Silk Slim Joggers",
+    price5: 899,
+
+    id: "women",
+    image6: "images/ede596ec-8c52-4f11-9210-df139900cec21540883446206-Mitera-Black-Silk-Blend-Banarasi-Ethnic-Solid-Woven-Saree-with-Matching-Blouse-8411540883445971-1.webp",
+    name6: "Solid Banarasi Saree",
+    price6: 2359,
+  },
+  {
+    id: "kid",
+    image1: "images/f4edb564-c5a9-4292-b47a-00464c86f64f1579732582431-1.webp",
+    name1: "Clean Look Jeans",
+    price1: 900,
+
+    id: "kid",
+    image2: "images/ac850d4b-8e6e-4067-b7eb-4ea9e961e4d11534486603914-StyleStone-Girls-Blue-Printed-Fit-and-Flare-Dress-1311534486603825-1.webp",
+    name2: "Printed Flare Dress",
+    price2: 800,
+
+    id: "kid",
+    image3: "images/0382daed-ffff-48b9-879f-4139dc0901591597639846624-Sweet-Dreams-Boys-Peach-Coloured--Green-Printed-Night-suit-2-1.jpg",
+    name3: "Printed Night Suit",
+    price3: 999,
+
+    id: "kid",
+    image4: "images/64d68229-ade1-431c-8bc3-422486d8a6b31594975298137-YK-Basics-Boys-Pack-of-5-Round-Neck-T-shirts-223159497529597-1.webp",
+    name4: "5 rounded T-shirt",
+    price4: 4299,
+
+    id: "kid",
+    image5: "images/22e77ac8-1a1a-4bdb-9c5c-eb5a84daa7cd1598617208585LilPicksBoysPinkSolidStraightKurta1.webp",
+    name5: "Solid Straight Kurta",
+    price5: 899,
+
+    id: "kid",
+    image6: "images/a5832b21-6c11-4076-8f46-3e4f3287bd5d1546649284259-1.webp",
+    name6: "Printed Kurta and Plazzos ",
+    price6: 2359,
+  },
+  {
+    id: "footwear",
+    image1: "images/1c077b32-6f5b-4776-b97c-55019b311ded1576578225554-Anouk-Women-Off-White-Beaded-Mojaris-2241576578224391-1.webp",
+    name1: "Off-White Beaded Mojaris",
+    price1: 900,
+
+    id: "footwear",
+    image2: "images/5fd89d7d-31e7-49cd-ad6f-f987182fd13e1582783276085-Skechers-Women-Navy-Blue--Pink-GO-RUN-RIDE-8-Running-Shoes-9-1.webp",
+    name2: "Pink GO RIDE Running Shoes",
+    price2: 800,
+
+    id: "footwear",
+    image3: "images/8d1bb62a-a4c8-4339-bbda-9a49c5d9d6f71559973009336-Crew-STREET-Women-Pink-Running-Shoes-9841559973008284-1.jpg",
+    name3: "Colourblocked Running Shoes",
+    price3: 999,
+
+    id: "footwear",
+    image4: "images/e00d86e1-7e0c-46d7-8e01-1c721ca06e281590079932688PumaMenNavyBlueRedEscaperStarIDPSneakers1.webp",
+    name4: " Blue & Red Escaper Star IDP Sneakers",
+    price4: 4299,
+
+    id: "footwear",
+    image5: "images/52f6a65d-301e-456e-b4d4-851255db49091572344975999-MetallicSquare-ToeSandals-221572344975143-1.webp",
+    name5: "Transparent Solid Sandals",
+    price5: 899,
+
+    id: "footwear",
+    image6: "images/9155a144-fad7-488b-a338-58c50bb844c61579848278081-Nike-Men-Red-AIR-MAX-IMPACT-Basketball-Shoes-704157984827574-1.webp",
+    name6: "Mid-Top Basketball Shoes",
+    price6: 2359,
+  },
+];
+
+let currentItem = 0;
+const men = document.querySelector(".men");
+const women = document.querySelector(".women");
+const kid = document.querySelector(".kid");
+const footwear = document.querySelector(".footwear");
+const navbar = document.querySelector(".head");
+const carousel = document.getElementById("Home");
+const topLink = document.querySelector(".top-link");
+
+
+window.addEventListener("scroll", function () {
+  let scrollHeight = window.pageYOffset;
+  let carouselHeight = carousel.getBoundingClientRect().height;
+  if (scrollHeight > carouselHeight) {
+    navbar.classList.add("fixed-navbar");
+  } else {
+    navbar.classList.remove("fixed-navbar");
+  }
+
+  //*******back to top icon visibilty***********
+  if (scrollHeight > 1150) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+//**********all variables***************** */
+
+//************selecting image************
+let pic1 = document.querySelector(".img1");
+let pic2 = document.querySelector(".img2");
+let pic3 = document.querySelector(".img3");
+let pic4 = document.querySelector(".img4");
+let pic5 = document.querySelector(".img5");
+let pic6 = document.querySelector(".img6");
+//**************selecting h6***************
+let picName1 = document.querySelector(".pro1");
+let picName2 = document.querySelector(".pro2");
+let picName3 = document.querySelector(".pro3");
+let picName4 = document.querySelector(".pro4");
+let picName5 = document.querySelector(".pro5");
+let picName6 = document.querySelector(".pro6");
+//*************selecting price**************
+let price1 = document.querySelector(".price1");
+let price2 = document.querySelector(".price2");
+let price3 = document.querySelector(".price3");
+let price4 = document.querySelector(".price4");
+let price5 = document.querySelector(".price5");
+let price6 = document.querySelector(".price6");
+
+//**************picselector*****************
+function picSelector(item) {
+  pic1.src = item.image1;
+  pic2.src = item.image2;
+  pic3.src = item.image3;
+  pic4.src = item.image4;
+  pic5.src = item.image5;
+  pic6.src = item.image6;
+}
+//***************h6 text selector************
+function picNameSelector(item) {
+  picName1.innerHTML = item.name1;
+  picName2.innerHTML = item.name2;
+  picName3.innerHTML = item.name3;
+  picName4.innerHTML = item.name4;
+  picName5.innerHTML = item.name5;
+  picName6.innerHTML = item.name6;
+}
+//*******************men tab*****************
+men.addEventListener("click", function () {
+  currentItem = 0;
+  let item = arrivals[currentItem];
+  picSelector(item);
+  picNameSelector(item);
+});
+//*****************women tab**************
+women.addEventListener("click", function () {
+  currentItem = 1;
+  let item = arrivals[currentItem];
+  picSelector(item);
+  picNameSelector(item);
+});
+//*******************kid tab****************
+kid.addEventListener("click", function () {
+  currentItem = 2;
+  let item = arrivals[currentItem];
+  picSelector(item);
+  picNameSelector(item);
+});
+//*******************footwear tab*************
+footwear.addEventListener("click", function () {
+  currentItem = 3;
+  let item = arrivals[currentItem];
+  picSelector(item);
+  picNameSelector(item);
+});
+
+const signin = document.querySelector(".sign-in");
+const modalbg = document.querySelector(".modal-bg")
+const cross = document.querySelector(".cross");
+signin.addEventListener("click", function () {
+  modalbg.classList.add("modal-active");
+});
+
+cross.addEventListener("click", function () {
+  modalbg.classList.remove("modal-active");
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target == modalbg) {
+    modalbg.classList.remove("modal-active")
+    console.log(event)
+  }
+})
---
+++ script2.js
@@ -0,0 +1,257 @@
+// *************products data**********//
+const products = [{
+        id: "men",
+        image1: "images/a8acac3a-077a-400f-a1c7-519215a5265e1537251707732-Roadster-Men-Blue-Denim-Washed-Casual-Shirt-5281537251705552-1.jpg",
+        name1: "Casual Denim Shirt",
+        price1: 900,
+        inCart1: 0,
+
+        // id: "women",
+        // image2: "images/8df4ed41-1e43-4a0d-97fe-eb47edbdbacd1556086871124-Libas-Women-Kurtas-6161556086869769-1.webp",
+        // name2: "Print Straight Kurta",
+        // price2: 900,
+        // inCart2: 0,
+
+        // id: "kid",
+        // image3: "images/f4edb564-c5a9-4292-b47a-00464c86f64f1579732582431-1.webp",
+        // name3: "Clean Look Jeans",
+        // price3: 900,
+        // inCart3: 0,
+
+        // id: "footwear",
+        // image4: "images/1c077b32-6f5b-4776-b97c-55019b311ded1576578225554-Anouk-Women-Off-White-Beaded-Mojaris-2241576578224391-1.webp",
+        // name4: "Off-White Beaded Mojaris",
+        // price4: 900,
+        // inCart4: 0, 
+    },
+    {
+        id: "men",
+        image1: "Images/60785496-7974-463b-b604-634d33ce6cde1599120622296-BEAT-LONDON-by-PEPE-JEANS-Men-Jeans-6291599120620590-1.jpg",
+        name1: "Slim Fit Jeans",
+        price1: 800,
+        inCart1: 0,
+
+        // id: "women",
+        // image2: "images/11522735513998-HRX-by-Hrithik-Roshan-Black-Joggers-2821522735513847-1.jpg",
+        // name2: "Lenght Track Pants",
+        // price2: 800,
+        // inCart2: 0,
+
+        // id: "kid",
+        // image3: "images/ac850d4b-8e6e-4067-b7eb-4ea9e961e4d11534486603914-StyleStone-Girls-Blue-Printed-Fit-and-Flare-Dress-1311534486603825-1.webp",
+        // name3: "Printed Flare Dress",
+        // price3: 800,
+        // inCart3: 0,
+
+        // id: "footwear",
+        // image4: "images/5fd89d7d-31e7-49cd-ad6f-f987182fd13e1582783276085-Skechers-Women-Navy-Blue--Pink-GO-RUN-RIDE-8-Running-Shoes-9-1.webp",
+        // name4: "Pink GO RIDE Running Shoes",
+        // price4: 800,
+        // inCart4: 0,
+    },
+    {
+        id: "men",
+        image1: "Images/7b9db4fb-317c-4ce9-ae30-e7522a89911d1568451555200-HERENOW-Men-Sweatshirts-6561568451553655-1.webp",
+        name1: "Printed Sweatshirts",
+        price1: 999,
+        inCart1: 0,
+
+        // id: "women",
+        // image2: "images/f979e44a-d583-447c-bfa4-cfa354fe18621569581591523-HRX-by-Hrithik-Roshan-Women-Tshirts-1641569581590485-1.webp",
+        // name2: "Running T-shirt",
+        // price2: 999,
+        // inCart2: 0,
+
+        // id: "kid",
+        // image3: "images/0382daed-ffff-48b9-879f-4139dc0901591597639846624-Sweet-Dreams-Boys-Peach-Coloured--Green-Printed-Night-suit-2-1.jpg",
+        // name3: "Printed Night Suit",
+        // price3: 999,
+        // inCart3: 0,
+
+        // id: "footwear",
+        // image4: "images/8d1bb62a-a4c8-4339-bbda-9a49c5d9d6f71559973009336-Crew-STREET-Women-Pink-Running-Shoes-9841559973008284-1.jpg",
+        // name4: "Colourblocked Running Shoes",
+        // price4: 999,
+        // inCart4: 0,
+
+
+    },
+    {
+        id: "men",
+        image1: "images/db444c48-c8aa-4fc3-92b4-02b66f4643dd1545820130218-Armani-Exchange-Men-Black-Analogue-Watch-6211545820128996-1.webp",
+        name1: "Analog Watch",
+        price1: 4299,
+        inCart1: 0,
+
+        // id: "women",
+        // image2: "images/e73eebe5-b63f-4643-a2d6-cbf39fdaec6f1570882139845-DressBerry-Women-Coats-1761570882137262-1.jpg",
+        // name2: "Mustard Solid Coat",
+        // price2: 4299,
+        // inCart2: 0,
+
+        // id: "kid",
+        // image3: "images/64d68229-ade1-431c-8bc3-422486d8a6b31594975298137-YK-Basics-Boys-Pack-of-5-Round-Neck-T-shirts-223159497529597-1.webp",
+        // name3: "5 rounded T-shirt",
+        // price3: 4299,
+        // inCart3: 0,
+
+        // id: "footwear",
+        // image4: "images/e00d86e1-7e0c-46d7-8e01-1c721ca06e281590079932688PumaMenNavyBlueRedEscaperStarIDPSneakers1.webp",
+        // name4: " Blue & Red Escaper Star IDP Sneakers",
+        // price4: 4299,
+        // inCart4: 0,
+    },
+    {
+        id: "men",
+        image1: "Images/11449479796670-INVICTUS-Red--Navy-Checked-Slim-Formal-Shirt-4621449479796242-1.webp",
+        name1: "Checked Formal Shirt",
+        price1: 899,
+        inCart1: 0,
+
+        // id: "women",
+        // image2: "images/b64bbae7-f220-46e3-9246-aad5aaab69171579040040721-1.webp",
+        // name2: "Silk Slim Joggers",
+        // price2: 899,
+        // inCart2: 0,
+
+        // id: "kid",
+        // image3: "images/22e77ac8-1a1a-4bdb-9c5c-eb5a84daa7cd1598617208585LilPicksBoysPinkSolidStraightKurta1.webp",
+        // name3: "Solid Straight Kurta",
+        // price3: 899,
+        // inCart3: 0,
+
+        // id: "footwear",
+        // image4: "images/52f6a65d-301e-456e-b4d4-851255db49091572344975999-MetallicSquare-ToeSandals-221572344975143-1.webp",
+        // name4: "Transparent Solid Sandals",
+        // price4: 899,
+        // inCart4: 0,
+    },
+    {
+        id: "men",
+        image1: "Images/d0945f5f-bb57-45a6-9615-7aea74707d201586861228235-ADIDAS-Men-Navy-Sedna-Running-Shoes-7821586861226106-1.webp",
+        name1: "Running Shoes",
+        price1: 2359,
+        inCart1: 0,
+
+        // id: "women",
+        // image2: "images/ede596ec-8c52-4f11-9210-df139900cec21540883446206-Mitera-Black-Silk-Blend-Banarasi-Ethnic-Solid-Woven-Saree-with-Matching-Blouse-8411540883445971-1.webp",
+        // name2: "Solid Banarasi Saree",
+        // price2: 2359,
+        // inCart2: 0,
+
+        // id: "kid",
+        // image3: "images/a5832b21-6c11-4076-8f46-3e4f3287bd5d1546649284259-1.webp",
+        // name3: "Printed Kurta and Plazzos ",
+        // price3: 2359,
+        // inCart3: 0,
+
+        // id: "footwear",
+        // image4: "images/9155a144-fad7-488b-a338-58c50bb844c61579848278081-Nike-Men-Red-AIR-MAX-IMPACT-Basketball-Shoes-704157984827574-1.webp",
+        // name4: "Mid-Top Basketball Shoes",
+        // price4: 2359,
+        // inCart4: 0,
+    },
+];
+let carts = document.querySelectorAll(".pro-p")
+let cartCount = document.querySelector(".cart-count")
+let cartTotal = document.querySelector(".cart-total")
+const clearCart = document.querySelector(".clear-cart")
+
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener("click", function () {
+        alert("Item added to cart")
+        cartNumbers(products[i]);
+        totalCost(products[i])
+    })
+}
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem("cartNumbers");
+
+    if (productNumbers) {
+        cartCount.textContent = productNumbers;
+
+    }
+}
+
+function cartNumbers(product) {
+
+    let productNumbers = localStorage.getItem("cartNumbers")
+
+    productNumbers = parseInt(productNumbers);
+
+    if (productNumbers) { //if product number is exist at first (nan) so that is bascially not a number//
+        localStorage.setItem("cartNumbers", productNumbers + 1);
+        cartCount.textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem("cartNumbers", 1);
+        cartCount.textContent = 1;
+    }
+    setItems(product);
+}
+
+function setItems(product) {
+    let cartItems = localStorage.getItem("productsIncart");
+    cartItems = JSON.parse(cartItems)
+    if (cartItems != null) {
+        if (cartItems[product.name1] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.name1]: product
+            }
+        }
+        cartItems[product.name1].inCart1 += 1;
+    } else {
+        product.inCart1 = 1;
+        cartItems = {
+            [product.name1]: product
+        }
+    }
+    localStorage.setItem("productsIncart", JSON.stringify(cartItems))
+}
+
+function totalCost(product) {
+    let cartCost = localStorage.getItem("totalCost");
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost)
+        localStorage.setItem("totalCost", cartCost + product.price1);
+    } else {
+        localStorage.setItem("totalCost", product.price1)
+    }
+}
+// ``=back ticks just so we can inject variable inside with string
+function displayCart() {
+    let cartItems = localStorage.getItem("productsIncart")
+    cartItems = JSON.parse(cartItems);
+    // console.log(cartItems)
+    let cartRow = document.querySelector(".cart-row");
+    if (cartItems && cartRow) {
+        cartRow.innerHTML = "";
+        Object.values(cartItems).map(function (item) {
+            cartRow.innerHTML += `
+     <div class = "cart-item col-md-3 col-3 cart-img">
+        <img src = "${item.image1}" alt = "">
+        </div> 
+        <div class = "col-lg-6 col-md-6 col-6">
+        <h5> ${item.name1} </h5> 
+        <h6> Rs. ${item.price1} </h6> 
+        <span class = "remove-item"> remove </span> 
+        </div>
+        <div class = "col-md-3 col-3" ><div>
+          <i style = "color: #4cd3c2" class = "fas fa-chevron-up"> </i> 
+          <p class = "item-amount" >${item.inCart1}</p> 
+          <i style = "color: #4cd3c2" class = "fas fa-chevron-down" > </i> 
+          </div> 
+        </div>
+      `
+        });
+
+        cartTotal.innerHTML = localStorage.getItem("totalCost");
+    }
+}
+onLoadCartNumbers();
+displayCart();
+clearCart.addEventListener("click", function () {
+    localStorage.clear();
+    alert("Refresh the page");
+});
